bf7e286c890892fa2b72145c7867ddb3b9aa8978
kztech <korczaktechnology@gmail.com>
2026-09-14 20:54:45 -0300

    style(desktop): apply configuration terminology consistently

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -11,4 +11,5 @@ import './PreparedPremium.css';
 import './GlobalPremium.css';
 import './FinalUIPolish.css';
 import './F2MasterDataProfessional.css';
+import './ConfigurationTerminology.css';
 mount();
